4a9085d3a66cb555079f424140e5c886ec4387c0
Joseph Peterson <Joseph.Peterson@jhuapl.edu>
2020-07-07 02:43:41 -0400

    Initial commit

---
+++ app/controllers/stock_quotes_controller.rb
@@ -0,0 +1,7 @@
+class StockQuotesController < ApplicationController
+  def index
+    @state = params[:state] || 'md'
+    @reps = Representative.for(@state.upcase!).
+      sort_by {|rep| rep['name']}
+  end
+end
