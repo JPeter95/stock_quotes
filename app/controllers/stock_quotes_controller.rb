c4b55109f1376913e423ae354031b4ac5e32f973
Joseph Peterson <Joseph.Peterson@jhuapl.edu>
2020-07-07 04:50:30 -0400

    Starting fresh

--- app/controllers/stock_quotes_controller.rb
+++
@@ -1,7 +0,0 @@
-class StockQuotesController < ApplicationController
-  def index
-    @state = params[:state] || 'md'
-    @reps = Representative.for(@state.upcase!).
-      sort_by {|rep| rep['name']}
-  end
-end
